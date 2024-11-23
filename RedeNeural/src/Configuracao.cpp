62ee8cf8823a3329b6c5514efe5c761c248a644a
Brunexgmaer09 <39810796+Brunexgmaer09@users.noreply.github.com>
2024-11-22 21:03:27 -0300

    Update Configuracao.cpp

--- RedeNeural/src/Configuracao.cpp
+++ RedeNeural/src/Configuracao.cpp
@@ -8,11 +8,11 @@ float ConfiguracaoNEAT::COEF_PESO = 0.4f;
 
 float ConfiguracaoNEAT::CHANCE_PESO_PERTURBADO = 0.8f;
 float ConfiguracaoNEAT::CHANCE_CONEXAO_TOGGLE = 0.15f;
-float ConfiguracaoNEAT::CHANCE_NOVO_NO = 0.2f;
-float ConfiguracaoNEAT::CHANCE_NOVA_CONEXAO = 0.25f;
+float ConfiguracaoNEAT::CHANCE_NOVO_NO = 0.9f;
+float ConfiguracaoNEAT::CHANCE_NOVA_CONEXAO = 0.9f;
 
-int ConfiguracaoNEAT::MAX_NOS = 1000;
-int ConfiguracaoNEAT::MAX_CONEXOES = 2000;
+int ConfiguracaoNEAT::MAX_NOS = 10;
+int ConfiguracaoNEAT::MAX_CONEXOES = 20;
 
 void ConfiguracaoNEAT::inicializarPadrao() {
     COEF_EXCESSO = 1.0f;
@@ -21,11 +21,11 @@ void ConfiguracaoNEAT::inicializarPadrao() {
     
     CHANCE_PESO_PERTURBADO = 0.8f;
     CHANCE_CONEXAO_TOGGLE = 0.15f;
-    CHANCE_NOVO_NO = 0.2f;
-    CHANCE_NOVA_CONEXAO = 0.25f;
+    CHANCE_NOVO_NO = 0.9f;
+    CHANCE_NOVA_CONEXAO = 0.9f;
     
-    MAX_NOS = 1000;
-    MAX_CONEXOES = 2000;
+    MAX_NOS = 10;
+    MAX_CONEXOES = 20;
 }
 
 } // namespace NEAT 
