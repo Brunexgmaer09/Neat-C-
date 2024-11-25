f45e9f2a51366715a19e88e072cef1b44dfb3296
Brunexgmaer09 <brunooliveiirah@gmail.com>
2024-11-25 16:48:17 -0300

    Melhorias na implementação NEAT: Adicionado sistema de logs, melhorado sistema de mutação e diversidade genétic

--- RedeNeural/HEADERS/Especie.hpp
+++ RedeNeural/HEADERS/Especie.hpp
@@ -3,5 +3,25 @@
 #include <vector>
 
 namespace NEAT {
-    // ... resto do código ...
-} 
+
+class Especie {
+private:
+    std::vector<Rede*> membros;
+    float aptidaoAjustada;
+    Rede representante;
+    int geracoesSemMelhoria;
+    float melhorAptidao;
+
+public:
+    Especie(const Rede& primeiro);
+    
+    void adicionarMembro(Rede* rede);
+    void calcularAptidaoAjustada();
+    bool verificarCompatibilidade(const Rede& rede) const;
+    
+    float obterAptidaoAjustada() const { return aptidaoAjustada; }
+    const std::vector<Rede*>& obterMembros() const { return membros; }
+    void limparMembros() { membros.clear(); }
+};
+
+} // namespace NEAT 
